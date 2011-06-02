f1884358238141e2904f08c755db6d94920fb739
Sean Upton <sdupton@gmail.com>
2011-06-02 15:07:08 -0600

    Removed RPC Auth plugin for repoze.who integration: not in use.

--- Products/qi/__init__.py
+++ Products/qi/__init__.py
@@ -1,25 +1,10 @@
+import os
+
 from zope.i18nmessageid import MessageFactory
+
+
 MessageFactory = MessageFactory('Products.qi')
 
-#from Products.qi.util.auth import whoplugin
-import os
-from django.conf import settings
-#import qi.sqladmin.settings
-from Products.PluggableAuthService.PluggableAuthService import \
-     registerMultiPlugin
-#registerMultiPlugin(whoplugin.WhoPlugin.meta_type)
-os.environ['DJANGO_SETTINGS_MODULE']='qi.sqladmin.settings'
 
-def initialize(context):
-    """ Initialize the product """
-    pass
-    """
-    from AccessControl.Permissions import manage_users
-    context.registerClass(whoplugin.WhoPlugin,
-                          permission = manage_users,
-                          constructors = (whoplugin.manage_addWhoPluginForm,
-                                          whoplugin.manage_addWhoPlugin,),
-                          visibility=None)
-    
-    #try registering all projects with the database?"""
-    
+os.environ['DJANGO_SETTINGS_MODULE'] = 'qi.sqladmin.settings'
+
