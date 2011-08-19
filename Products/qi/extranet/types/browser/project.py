acc8d302b102fda76b0f53b39679967f734d023c
Sean Upton <sdupton@gmail.com>
2011-08-19 16:21:49 -0600

    cruft removal: Products.qi.extranet.types.browser.project -- remove ununsed imports and commented-out code

--- Products/qi/extranet/types/browser/project.py
+++ Products/qi/extranet/types/browser/project.py
@@ -1,41 +1,18 @@
-
 from zope.component import createObject
 from zope.formlib import form
 
-from Products.MailHost.interfaces import IMailHost
-from zope.component import getUtility
-
-from Acquisition import aq_inner
-from zExceptions import BadRequest
 from Products.Five.browser import BrowserView
-from Products.qi.util.general import BrowserPlusView
-from Products.CMFCore.utils import getToolByName
-from plone.portlets.manager import PortletManager
-from DateTime import DateTime
 
 from plone.app.form import base
-from plone.app.form.widgets.uberselectionwidget import UberMultiSelectionWidget
-#from plone.app.vocabularies.users import UsersSource
-from Products.qi.extranet.users.qiuser import QiUsersSource as UsersSource
-from plone.app.vocabularies.users import UsersSourceQueryView
 
 from Products.qi.extranet.types.interfaces import IQIProject
 from Products.qi import MessageFactory as _
-from Products.qi.extranet.types.handlers.users import add_managers_and_faculty
-from qi.sqladmin.models import Project as RelProject
-from qi.sqladmin import models as DB
-from datetime import datetime
 
 from Products.qi.extranet.types.handlers import django as dbhandlers
 from Products.qi.extranet.types import project as content
-from Products.qi.util.utils import getTeamsInContext
-from Products.CMFPlone.patches.unicodehacks import _unicode_replace
-from Products.qi.util import utils
 
 
 project_form_fields = form.Fields(IQIProject)
-#project_form_fields['managers'].custom_widget = UberMultiSelectionWidget
-#project_form_fields['faculty'].custom_widget = UberMultiSelectionWidget
 
 class ProjectAddForm(base.AddForm):
     """Add form for projects
