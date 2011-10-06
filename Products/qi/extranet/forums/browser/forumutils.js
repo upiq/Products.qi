755f051e6a5462e3ee80f9edb181673a211b6aed
Sean Upton <sdupton@gmail.com>
2011-10-06 11:52:09 -0600

    removal of forums from Products.qi -- we are not using this.

--- Products/qi/extranet/forums/browser/forumutils.js
+++
@@ -1,8 +0,0 @@
-function addattachment()
-{
-    addedto=document.getElementById("attachments");
-    addedElement=document.createElement("div");
-    addedElement.innerHTML="<input type=\"file\" style=\"width:300px;\" name=\"attachment\"/>"
-    addedto.appendChild(addedElement)
-    //TODO: add some boring DOM trickery
-}
