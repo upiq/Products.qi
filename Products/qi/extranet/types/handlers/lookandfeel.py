8f1a4b83fbc027f0796d71061c237fbca336c348
Sean Upton <sdupton@gmail.com>
2011-10-13 19:09:44 -0600

    removed obsoleted handler for setting project (CMF) skin.

--- Products/qi/extranet/types/handlers/lookandfeel.py
+++ Products/qi/extranet/types/handlers/lookandfeel.py
@@ -1,21 +1,6 @@
 from Products.qi.extranet.types.interfaces import _PROJECT_LOGO_NAME
 
 
-def setProjectSkin(obj, event):
-    #print vars(event)
-    other=event.request.other
-    try:
-        lastparent=other['PARENTS'][-1]
-        objectname=other['TraversalRequestNameStack'][-1]
-    except:
-        return
-    if hasattr(lastparent, objectname):
-        realobj=getattr(lastparent,objectname)
-        if hasattr(realobj,'projectTheme'):
-            theme=realobj.projectTheme
-            obj.changeSkin(theme,event.request)
-
-
 def manage_project_logo(project, event):
     # bounce the formdata .logo field over to a logo subobject on add or edit
     if project.logo:
