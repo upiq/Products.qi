4a7e357c25e6b5be00be216432af834900afe691
Sean Upton <sdupton@gmail.com>
2011-10-14 00:28:23 -0600

    removed viewlet override for plone.htmlhead.title viewlet, this is obsoleted by Plone 3.3+ INavigationRoot support that is (and has been for a while) stock in plone.

--- Products/qi/extranet/viewlets/viewlets.py
+++ Products/qi/extranet/viewlets/viewlets.py
@@ -1,13 +1,9 @@
 from Products.Five.browser import BrowserView
 from zope.viewlet.interfaces import IViewlet
 from zope.interface import implements
-from plone.app.layout.viewlets import common
-from Products.qi.extranet.types.project import Project
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.qi.extranet.types.interfaces import _PROJECT_LOGO_NAME
 from zope.component import getMultiAdapter
-from Products.CMFPlone.utils import safe_unicode
-from cgi import escape
+
 
 class LogoViewlet(BrowserView):
 
@@ -45,46 +41,4 @@ class LogoViewlet(BrowserView):
         else:
             homelink=self.context.absolute_url()
         return hometag%(homelink, logo.tag())
-
- 
-class TitleViewlet(common.ViewletBase):
-
-    def update(self):
-        self.portal_state = getMultiAdapter((self.context, self.request),
-                                            name=u'plone_portal_state')
-        self.context_state = getMultiAdapter((self.context, self.request),
-                                             name=u'plone_context_state')
-        self.page_title = self.context_state.object_title
-        self.portal_title = self.portal_state.portal_title
-
-    def render(self):
-        portal_title = safe_unicode(self.portal_title())
-        page_title = safe_unicode(self.page_title())
-        if page_title == portal_title:
-            return u"<title>%s</title>" % (escape(portal_title))
-        elif not hasattr(self.context, 'getProject'):
-            return u"<title>%s &mdash; %s</title>" % (
-                escape(safe_unicode(page_title)),
-                escape(safe_unicode(portal_title)))
-        elif not isinstance(self.context.aq_parent.aq_parent, Project):
-            return u"<title>%s &mdash; %s</title>" % (
-                escape(safe_unicode(page_title)),
-                escape(safe_unicode(self.context.getProject().title)))
-        else:
-            return "<title>%s</title>"%(escape(self.context.getProject().title))
-            
-            
-class UtilitiesView:
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-
-    def domainStrip(self, url):
-        # AC 2008-04-08 it aquires the domain name being used by the
-        # project in order to determine if the second item on the breadcrumbs 
-        # is needed
-        urlChunks=url.split('/')
-        domain=urlChunks[2]
-        domain=domain.split(':')[0]
-        return domain
 
