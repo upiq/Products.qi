e0d8ac2c7228c1803b6054a82ed498103e3dca22
Sean Upton <sdupton@gmail.com>
2013-08-06 11:56:57 -0600

    all workspaces, not just projects, can have start/end dates; trailing whitespace tweaks

--- Products/qi/extranet/types/interfaces.py
+++ Products/qi/extranet/types/interfaces.py
@@ -32,12 +32,12 @@ class UTF8LinesConverter(TextLinesConverter):
     by encodings (though values for these are usually ASCII, casting alone
     should be considered too implicit and too prone to break).
     """
-    
+
     def toWidgetValue(self, value):
         if value and isinstance(value[0], str):
             value = list(element.decode('utf-8') for element in value)
         return super(UTF8LinesConverter, self).toWidgetValue(value)
-    
+
     def toFieldValue(self, value):
         collection_type = self.field._type
         value_type = self.field.value_type._type
@@ -55,16 +55,30 @@ class IWorkspace(form.Schema, IOrderedContainer, IAttributeUUID):
     be accomplished via adaptation -- this interface does
     not mandate a specific interface for handling that).
     """
-    
+
     title = schema.TextLine(
         title=_(u'Title'),
         description=_(u'Workspace name or display title.'),
         required=True)
-    
+
     description = schema.Text(
         title=_(u'Description'),
         description=(u'Project description; may be displayed for viewers '
                      u'of project.'),
+        required=False,
+        )
+
+    form.widget(start=SmartdateFieldWidget)
+    start = schema.Date(
+        title=_(u'Start date'),
+        description=_(u'Date collaboration in workspace is to begin.'),
+        required=False,
+        )
+
+    form.widget(end=SmartdateFieldWidget)
+    end = schema.Date(
+        title=_(u'End date'),
+        description=_(u'Date collaboration in workspace is to end.'),
         required=False,
         )
 
@@ -73,27 +87,13 @@ class IProject(IWorkspace):
     """
     Project is a top-level folder with project workspace.
     """
-    
+
     form.fieldset(
         'configuration',
         label=_(u'Configuration'),
         fields=['start', 'stop', 'contact', 'logo'],
         )
-    
-    form.widget(start=SmartdateFieldWidget)
-    start = schema.Date(
-        title=_(u'Project start'),
-        description=_(u'Date project begins.'),
-        required=False,
-        )
-   
-    form.widget(end=SmartdateFieldWidget)
-    end = schema.Date(
-        title=_(u'Project end'),
-        description=_(u'Date project ends.'),
-        required=False,
-        )
-    
+
     form.widget(contacts=textlines.TextLinesFieldWidget)
     contacts = schema.List(
         title=_(u'Contact email'),
@@ -102,13 +102,13 @@ class IProject(IWorkspace):
         defaultFactory=list,    # requires zope.schema >= 3.8.0
         required=False,
         )
-    
+
     logo = NamedImage(
         title=_(u'Project logo'),
         description=_(u'Upload a project logo file as PNG or JPEG image.'),
         required=False,
         )
-    
+
     @invariant
     def start_end_valid_range(data):
         if data.end and data.start:
@@ -118,10 +118,22 @@ class IProject(IWorkspace):
 
 class ITeam(IWorkspace):
     """Marker for any kind of team (including sub-team) workspace"""
- 
+
+    form.fieldset(
+        'configuration',
+        label=_(u'Configuration'),
+        fields=['start', 'stop'],
+        )
+
 
 class ISubTeam(ITeam):
     """Marker for a specifically sub-team workspace"""
+
+    form.fieldset(
+        'configuration',
+        label=_(u'Configuration'),
+        fields=['start', 'stop'],
+        )
 
 
 class IWorkspaceContext(Interface):
@@ -129,17 +141,17 @@ class IWorkspaceContext(Interface):
     Adapter interface providing the workspace context for projects and
     teams, given some location being adapted.
     """
-    
+
     def team():
         """
         Return containing team workspace for context or None
         """
-    
+
     def project():
         """
         Return containing project workspace for context or None
         """
-    
+
     def workspace():
         """
         Return containing project or team workspace for context or None.
