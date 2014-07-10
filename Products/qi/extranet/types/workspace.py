cc05297a1821307c901bd053622c1225eb6f14e9
Sean Upton <sdupton@gmail.com>
2014-07-10 14:50:07 -0600

    IPublishTraverse adapter to work around plone bug: https://dev.plone.org/ticket/14266 -- will allow users to create content in workspaces with same id/shortname as any workspace field name

--- Products/qi/extranet/types/workspace.py
+++ Products/qi/extranet/types/workspace.py
@@ -1,7 +1,30 @@
+from Acquisition import aq_base
+from zope.component import adapts
 from zope.interface import implements
+from zope.publisher.interfaces.browser import IBrowserRequest
 from plone.dexterity.content import Container
+from plone.dexterity.browser.traversal import DexterityPublishTraverse
 
 from Products.qi.extranet.types.interfaces import IWorkspace
+
+
+class WorkspacePublishTraverse(DexterityPublishTraverse):
+    """
+    Work-around https://dev.plone.org/ticket/14266 -- favor
+    contained content versus attributes for traversal.
+    """
+
+    adapts(IWorkspace, IBrowserRequest)
+
+    def publishTraverse(self, request, name):
+        marker = object()
+        attr_v = getattr(aq_base(self.context), name, marker)
+        if attr_v is not marker and name in self.context.objectIds():
+            # collision, only in this case favor the content:
+            return self.context.get(name)
+        # otherwise, no collision means business as usual:
+        default = super(WorkspacePublishTraverse, self).publishTraverse
+        return default(request, name)
 
 
 class Workspace(Container):
